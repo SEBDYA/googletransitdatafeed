7222e1d37c2417ed67c3c578c8628b3e72fa2165
tom.brown.code <tom.brown.code@705c8f7f-ad29-0410-9695-93dd3b14b492>
2008-05-06 16:09:48 +0000

    * Put utf8 back in good_feed * sync zip with directory of text files * fix utf8 weakness in google_random_queries which let me know that directory and zip were out of sync

--- branches/tom.brown.code-sync_zip_data/python/examples/google_random_queries.py
+++ branches/tom.brown.code-sync_zip_data/python/examples/google_random_queries.py
@@ -161,10 +161,12 @@ If you find a problem be sure to save the URL. This file is generated randomly.
 </html>
 """ % locals()
 
-  f.write(output_prefix)
+  f.write(transitfeed.EncodeUnicode(output_prefix))
   for source, destination in zip(locations[0:limit], locations[1:limit + 1]):
-    f.write("<li>" + LatLngsToGoogleLink(source, destination) + "\n")
-  f.write(output_suffix)
+    f.write(transitfeed.EncodeUnicode("<li>" +
+                                      LatLngsToGoogleLink(source, destination)
+                                      + "\n"))
+  f.write(transitfeed.EncodeUnicode(output_suffix))
 
 
 def ParentAndBaseName(path):
